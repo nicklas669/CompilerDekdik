d5c8e75952f90ce6bece4b4428909c0978008444
Stefan Hyltoft <stefanhyltoft@gmail.com>
2015-11-21 20:52:32 +0100

    ConstructorTest pushed - ingen fejl ??

---
+++ tests/typechecker/ConstructorTest.java
@@ -0,0 +1,15 @@
+class testmain {
+	
+	public static void main (String[] args) {
+		System.out.println("Hello World");
+		A constructTest = new A();
+		return;
+	}
+}
+
+class A {
+   
+   public A() {
+	   return;
+   }
+}
